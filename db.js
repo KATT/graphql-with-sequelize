06b78dee804cf863a72f3a978e8686959efe3d16
KATT <alexander@n1s.se>
2016-03-03 10:39:56 -0300

    create a lot of entries

--- db.js
+++ db.js
@@ -8,7 +8,8 @@ const Conn = new Sequelize(
   'postgres',
   {
     dialect: 'postgres',
-    host: 'localhost'
+    host: 'localhost',
+    logging: true
   }
 );
 
@@ -39,7 +40,7 @@ const Post = Conn.define('post', {
     allowNull: false
   },
   content: {
-    type: Sequelize.STRING,
+    type: Sequelize.TEXT,
     allowNull: false
   }
 });
@@ -49,25 +50,43 @@ Person.hasMany(Post);
 Post.belongsTo(Person);
 
 Conn.sync({ force: false }).then(async ()=> {
+  const nEntries = 100000;
   const count = await Person.count();
-  if (count > 0) {
-    return;
-  }
-  _.times(1000, ()=> {
-    return Person.create({
-      firstName: Faker.name.firstName(),
-      lastName: Faker.name.lastName(),
-      email: Faker.internet.email(),
-      age: Faker.random.number({max: 122})
-    }).then(person => {
-      _.times(_.random(0, 5), () => {
-        return person.createPost({
-          title: `Sample post by ${person.firstName}`,
-          content: 'here is some content'
-        });
+
+  const nEntriesToCreate = (nEntries - count);
+  
+  console.log(`${count} people in db, will create ${nEntriesToCreate} new ones`);
+
+  let batch = [];
+  const batchSize = 1000;
+  for (let i = 0; i < nEntriesToCreate; i++) {
+    const personPromise = (async () => {
+      const person = await Person.create({
+        firstName: Faker.name.firstName(),
+        lastName: Faker.name.lastName(),
+        email: Faker.internet.email(),
+        age: Faker.random.number({max: 122})
       });
-    });
-  });
+
+      const nPosts = _.random(0, 5);
+      for (let j = 0; j < nPosts; j++) {
+        await person.createPost({
+          title: `Sample post #${j+1} by ${person.firstName}`,
+          content: Faker.lorem.paragraphs()
+        });
+      }
+    })();
+
+    if (batch.length >= batchSize) {
+      await Promise.all(batch);
+      console.log(`created ${i} entries, ${nEntriesToCreate-i} left`);
+      batch = [];
+    }
+
+    batch.push(personPromise);
+  };
+
+  console.log('ALL DONE');
 });
 
 export default Conn;
