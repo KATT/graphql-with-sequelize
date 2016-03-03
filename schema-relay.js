252bec9bc4f670aa1958e75e0fe328298bf0caa1
KATT <alexander@n1s.se>
2016-03-03 09:40:23 -0300

    extract utils to own file

--- schema-relay.js
+++ schema-relay.js
@@ -6,8 +6,7 @@ import {
   GraphQLInt,
   GraphQLSchema,
   GraphQLList,
-  GraphQLInputObjectType,
-  GraphQLError
+  GraphQLInputObjectType
 } from 'graphql';
 
 
@@ -17,13 +16,21 @@ import {
   nodeDefinitions,
   fromGlobalId,
   globalIdField,
-  cursorToOffset,
   connectionArgs,
-  connectionDefinitions,
   connectionFromArray,
   connectionFromPromisedArray,
   connectionFromArraySlice
 } from 'graphql-relay';
+
+
+import {
+  connectionDefinitionWithCount,
+  getRelayQueryParams,
+  operatorsFieldInput,
+  getSelectedFieldsFromResolveInfo,
+  connectionFromDBMeta,
+  getAttributesList
+} from './lib/relay-sequelize';
 
 const { nodeInterface, nodeField } = nodeDefinitions(
   globalId => {
@@ -146,231 +153,6 @@ const { connectionType: postConnection } = connectionDefinitionWithCount({
   nodeType: Post,
 });
 
-// Helpers
-function connectionDefinitionWithCount(opts) {
-  const {connectionFields, ...other} = opts;
-
-
-  const newConnectionFields = Object.assign({}, connectionFields, {
-    count: {
-      type: GraphQLInt,
-      resolve: ({count}) => count,
-      description: 'A count of the total number of objects in this connection'
-    }
-  });
-
-  return connectionDefinitions({...other, connectionFields: newConnectionFields});
-}
-
-function getConditionsForField(fieldName, args) {
-  if (args.hasOwnProperty('eq')) {
-    return args.eq;
-  }
-
-  const conditions = {};
-  for (const operatorName in args) {
-    const value = args[operatorName];
-    
-    conditions['$' + operatorName] = value;
-  }
-
-  return conditions;
-}
-
-function getConditionsFromWhereArg(args) {
-  const where = {};
-  if (!args) {
-    return where;
-  }
-
-  for (const key in args) {
-    const conditions = args[key];
-
-    if (key === '_and') {
-      where.$and = getConditionsFromWhereArg(conditions);
-    } else if (key === '_or') {
-      where.$or = conditions.map(condition => getConditionsFromWhereArg(condition));
-    } else {
-      where[key] = getConditionsForField(key, conditions);
-    }
-  }
-
-  return where;
-}
-
-function cursorToArrayOffset(cursor, fieldName) {
-  if (!cursor) {
-    return 0;
-  }
-
-  const offset = cursorToOffset(cursor);
-
-  if (isNaN(offset) || offset < 0) {
-    throw new GraphQLError(badValueMessage(fieldName, GraphQLString, cursor));
-  }
-
-  return offset + 1;
-}
-
-function validateOnlyOneTruthy(args) {
-  let truthy = false;
-
-
-  Object.values(args).forEach(value => {
-    if (value) {
-      if (truthy) {
-        const keys = Object.keys(args);
-        throw new GraphQLError(`You can only use one of arguments: ${keys.join(', ')}.`);
-      }
-      truthy = true;
-    }
-  });
-}
-
-function validateUnsupportedArg(args, fieldName) {
-  if (args.hasOwnProperty(fieldName)) {
-    throw new Error(`Argument '${fieldName}' is currently unsupported.`);
-  }
-}
-
-function getLimit({first, last}) {
-  if (!first || !last) {
-    return 0;
-  }
-  validateOnlyOneTruthy({first, last});
-
-  // TODO support `last`
-
-  return first || last;
-}
-
-function getOffset({before, after}) {
-  validateOnlyOneTruthy({before, after})
-  
-  // TODO support `after`
-
-  return cursorToArrayOffset(after, 'after');
-}
-
-function getInclude(args, includeList) {
-  const include = [];
-  includeList.forEach((opts) => {
-    const {key, ...other} = opts;
-    if (args.hasOwnProperty(key)) {
-      const where = getConditionsFromWhereArg(args[key]);
-      const attributes = [];
-      include.push({
-        where,
-        attributes,
-        ...other,
-      });
-    }
-  });
-
-  return include;
-}
-
-function getRelayQueryParams(args, include = []) {
-  validateUnsupportedArg(args, 'before');
-  validateUnsupportedArg(args, 'last');
-
-  const query = {};
-
-  query.limit = getLimit(args);
-  query.offset = getOffset(args);
-  query.where = getConditionsFromWhereArg(args.where);
-  query.include = getInclude(args, include);
-
-  console.log('getRelayQueryParams query', query);
-
-  return query;
-}
-
-function getWhereInputFields(inputType) {
-  // http://docs.sequelizejs.com/en/latest/docs/querying/#operators
-  // TODO suport more types
-  const all = {
-    eq: {
-      type: inputType,
-    },
-    ne: {
-      type: inputType,
-    },
-    in: {
-      type: new GraphQLList(inputType),
-    },
-    notIn: {
-      type: new GraphQLList(inputType),
-    },
-  };
-
-  const map = {
-    'Int': () => ({
-      ...all,
-      lt: {
-        type: inputType,
-      },
-      lte: {
-        type: inputType,
-      },
-      gt: {
-        type: inputType,
-      },
-      gte: {
-        type: inputType,
-      },
-      between: {
-        type: new GraphQLList(inputType),
-      },
-      notBetween: {
-        type: new GraphQLList(inputType),
-      }
-    }),
-    'String': () => ({
-      ...all,
-      like: {
-        type: inputType,
-      },
-      iLike: {
-        type: inputType,
-      },
-      notLike: {
-        type: inputType,
-      },
-      notILike: {
-        type: inputType,
-      }
-    })
-  };
-
-  return map[inputType.name]();
-}
-
-const operatorsInputType = (function () {
-  const operatorTypes = {};
-
-  return (inputType) => {
-    const name = `Operators${inputType.name}Input`;
-
-    if (!operatorTypes[name]) {
-      const fields = getWhereInputFields(inputType);
-
-      operatorTypes[name] = new GraphQLInputObjectType({
-        name,
-        fields,
-      });
-    }
-
-    return operatorTypes[name];
-  };
-}());
-
-const operatorsFieldInput = function(inputType) {
-  return {
-    type: operatorsInputType(inputType),
-  };
-}
-
 const PostWhereInput = new GraphQLInputObjectType({
   name: 'PostWhereInput',
   fields: () => ({
@@ -394,60 +176,6 @@ const PersonWhereInput = new GraphQLInputObjectType({
   })
 });
 
-function getSelectedFieldsFromSelection(selection, path = []) {
-  const fields = new Set();
-
-  const {selectionSet} = selection;
-
-  if (selectionSet && selectionSet.selections.length > 0) {
-    selectionSet.selections.forEach(sub => {
-      fields.add([...path, sub.name.value].join('.'));
-
-      const newFields = getSelectedFieldsFromSelection(sub, [...path, sub.name.value]);
-
-      newFields.forEach(field => fields.add(field));
-    });
-  } else {
-    fields.add(path.join('.'));
-  }
-
-  return fields;
-}
-
-function getSelectedFieldsFromResolveInfo({fieldASTs}) {
-  const fields = new Set();
-
-  fieldASTs.forEach(selection => {
-    const newFields = getSelectedFieldsFromSelection(selection);
-
-    newFields.forEach(field => fields.add(field));
-  });
-
-  return Array.from(fields);
-}
-
-function connectionFromDBMeta({args, offset, count, rows}) {
-    const meta = {
-      sliceStart: offset,
-      arrayLength: count
-    };
-
-    const connection = connectionFromArraySlice(rows, args, meta);
-
-    const connectionWithCount = {...connection, count};
-
-    return connectionWithCount;
-}
-
-function getAttributesList(fields, whitelist, always = ['id']) {
-  const attributes = fields
-    .filter(field => field.startsWith('edges.node.'))
-    .map(field => field.replace('edges.node.', ''))
-    .filter(field => whitelist.includes(field))
-    ;
-
-  return [...always, ...attributes];
-}
 
 const queryType = new GraphQLObjectType({
   name: 'Query',
